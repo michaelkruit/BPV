7f04973359eabc8b9e70ad9dcb0c511b304eeed4
michaelkruit <michael_mikin@hotmail.com>
2017-06-13 16:04:12 +0200

    Added existing login to database

--- DeltaImpuls/Models/IdentityModels.cs
+++ DeltaImpuls/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace DeltaImpuls.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("datimpuls", throwIfV1Schema: false)
         {
         }
 
