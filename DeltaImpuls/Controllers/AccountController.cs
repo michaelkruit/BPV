30f4da8a87a79743210c183a1aa1fc3cd83e3d0f
michaelkruit <michael_mikin@hotmail.com>
2017-06-15 13:27:18 +0200

    Changend inlog error

--- DeltaImpuls/Controllers/AccountController.cs
+++ DeltaImpuls/Controllers/AccountController.cs
@@ -86,7 +86,7 @@ namespace DeltaImpuls.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Inlog gegevens zijn niet correct");
                     return View(model);
             }
         }
